5c9dc2a978031e9d942cc4fc560067bd3a198b1b
Rushi A <69522459+RushiAth@users.noreply.github.com>
2024-02-22 09:19:19 -0500

    finished home page styling

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,62 +1,68 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
-import { TextField, Typography, List, ListItem, ListItemText } from '@mui/material'
+import { Box, Typography, List, ListItem, ListItemText } from '@mui/material'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
-      <Typography variant='h2' component='h2' align='center' style={{ backgroundColor: '#2D9596', padding: '10px', marginTop: '10px', width: '50%'}}>
-        Code Translator
-      </Typography>
+      <Box display="flex" flexDirection='column' justifyContent="center" alignItems="center" minHeight="100vh">
+        <Typography variant='h2' component='h2' align='center' style={{ backgroundColor: '#2D9596', padding: '15px', width: '65%', borderRadius: '25px' , color: '#F1FADA', fontSize: '56px', fontStyle: 'normal', fontWeight: '600'}}>
+          Code Translator
+        </Typography>
 
-      <Typography paragraph='true' align='center' style={{ backgroundColor: '#2D9596', padding: '10px', marginTop: '10px', width: '50%'}}>
-        This tool uses AI models to convert your code from one programming language to another. This tool provides support for a variety of languages such as:
-        <List align='center' sx={{ listStyleType: 'disc', pl: 4 }}>
-          <ListItem sx={{ display: 'list-item' }}>
-            <ListItemText>C++</ListItemText>
-          </ListItem>
-          <ListItem sx={{ display: 'list-item' }}>
-            <ListItemText>C</ListItemText>
-          </ListItem>
-          <ListItem sx={{ display: 'list-item' }}>
-            <ListItemText>Java</ListItemText>
-          </ListItem>
-          <ListItem sx={{ display: 'list-item' }}>
-            <ListItemText>Python</ListItemText>
-          </ListItem>
-          <ListItem sx={{ display: 'list-item' }}>
-            <ListItemText>JavaScript</ListItemText>
-          </ListItem>
-          <ListItem sx={{ display: 'list-item' }}>
-            <ListItemText>Assembly</ListItemText>
-          </ListItem>
-        </List>
+        <Typography paragraph='true' style={{ backgroundColor: '#2D9596', padding: '25px', marginTop: '20px', marginBottom: '0px', width: '65%', borderRadius: '25px', color: '#F1FADA', fontSize: '28px', fontStyle: 'normal', fontWeight: '300'}}>
+          This tool uses an AI model to convert your code from one programming language to another. We provide support for a variety of languages such as:
 
-      </Typography>
-
-      <Typography paragraph='true' align='center' style={{ backgroundColor: '#2D9596', padding: '10px', marginTop: '10px', width: '50%' }}>
-        <h4>Getting Started:</h4>
-        <List sx={{ listStyleType: 'disc', pl: 4 }}>
-            <ListItem>
-              <ListItemText>Click on the Translate button in the NavBar above</ListItemText>
+          <List dense='true' disablePadding='true' sx={{ listStyleType: 'disc', pl: 4, marginTop: '5px', marginBottom: '0px'}}>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              C++
             </ListItem>
-            <ListItem>
-              <ListItemText>Select the language of the source code and the language of the translated code using the two dropdowns</ListItemText>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              C
             </ListItem>
-            <ListItem>
-              <ListItemText>Paste source code in the text editor</ListItemText>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Java
             </ListItem>
-            <ListItem>
-              <ListItemText>Click the translate button</ListItemText>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Python
             </ListItem>
-            <ListItem>
-              <ListItemText>Make sure to give positive/negative feedback</ListItemText>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              JavaScript
+            </ListItem>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Assembly
             </ListItem>
           </List>
-      </Typography>
+        </Typography>
+
+        <Typography paragraph='true' style={{ backgroundColor: '#2D9596', padding: '25px', marginTop: '20px', marginBottom: '20px', width: '65%', borderRadius: '25px', color: '#F1FADA', fontSize: '28px', fontStyle: 'normal', fontWeight: '300'}}>
+          <Typography variant='h4' component='h4' style={{ backgroundColor: '#2D9596', padding: '0px', margin: '0px', color: '#F1FADA', fontSize: '32px', fontStyle: 'normal', fontWeight: '550'}}>
+            Getting Started
+          </Typography>
+
+          <List dense='true' disablePadding='true' sx={{ listStyle: 'decimal', pl: 4, marginTop: '5px', marginBottom: '0px'}}>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Click on the Translate button in the NavBar above
+            </ListItem>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Select the language of the source code and the language of the translated code using the two dropdowns
+            </ListItem>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Paste source code in the text editor
+            </ListItem>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Click the translate button
+            </ListItem>
+            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+              Make sure to give positive/negative feedback!
+            </ListItem>
+          </List>
+        </Typography>
+      </Box>
+
     </>
   )
 }
