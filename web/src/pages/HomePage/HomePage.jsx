a049c6a4d42e165f51f39a1a1c93862f4b8eb4a2
TaherMAfini <tma29@njit.edu>
2024-02-22 18:31:24 -0500

    Remove testing buttons

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
-import { Metadata } from '@redwoodjs/web'
 import { useAuth } from 'src/auth'
 
 const HomePage = () => {
-  const { isAuthenticated, signUp, logIn, logOut, userMetadata } = useAuth()
+  const { isAuthenticated } = useAuth()
 
 
   return (
     <>
-      {/* MetaTags, h1, paragraphs, etc. */}
       <p>{JSON.stringify({ isAuthenticated })}</p>
-      { isAuthenticated && <p>Email: {userMetadata.email}</p>}
-      <button onClick={signUp}>sign up</button>
-      <button onClick={logIn}>Log In</button>
-      <button onClick={logOut}>Log Out</button>
     </>
   )
 }
