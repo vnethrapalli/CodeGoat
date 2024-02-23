c1a47a4c84d082d76e803ae84d65cc80c0847769
Rushi A <69522459+RushiAth@users.noreply.github.com>
2024-02-22 19:17:29 -0500

    Testing with Dark Mode Palette

--- web/src/layouts/UserLayout/UserLayout.jsx
+++ web/src/layouts/UserLayout/UserLayout.jsx
@@ -1,53 +1,69 @@
 import { routes } from '@redwoodjs/router'
 
-import { Link } from '@mui/material';
+import { AppBar, Link } from '@mui/material';
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import IconButton from '@mui/material/IconButton';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useTheme } from '@emotion/react';
 
 const themeLight = createTheme({
   palette: {
+    primary: {
+      main: "#F1FADA"
+    },
+    secondary: {
+      main: "#2D9596"
+    },
     background: {
       default: "#F1FADA"
+    },
+    text: {
+      primary: "#000000"
     }
   }
 });
 
 const themeDark = createTheme({
   palette: {
+    primary: {
+      main: "#F1FADA"
+    },
+    secondary: {
+      main: "#FFFFFF"
+    },
     background: {
       default: "#565656"
     },
     text: {
-      primary: "#ffffff"
+      primary: "#FFFFFF"
     }
   }
 });
 
 const UserLayout = ({ children }) => {
   const [light, setLight] = React.useState(true);
+  const theme = useTheme();
 
   return (
     <>
       <ThemeProvider theme={light ? themeLight : themeDark}>
         <CssBaseline />
-        <header className='relative flex'>
+        <AppBar position="static" sx={{ background: '#265073', marginBottom: '20px', height: '10%' }}>
           <h1>
-            <Link href={routes.home()} underline="none">
+            <Link href={routes.home()} underline="none" sx={{ marginLeft: "10px", color: "#F1FADA" }}>
               CodeGoat
             </Link>
           </h1>
 
-          <nav>
-            <IconButton sx={{ ml: 1 }} onClick={() => setLight((prev) => !prev)}>
-              {light === true ? <Brightness7Icon /> : <Brightness4Icon />}
-            </IconButton>
-          </nav>
-        </header>
+          <IconButton sx={{ ml: 1, width: '3%' }} onClick={() => setLight((prev) => !prev)}>
+            {light === true ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </AppBar>
+
+        <main>{children}</main>
       </ThemeProvider>
-      <main>{children}</main>
     </>
   )
 }
--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,38 +1,42 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
-import { Box, Typography, List, ListItem, ListItemText } from '@mui/material'
+import { Box, Typography, List, ListItem } from '@mui/material'
+import { useTheme } from '@mui/material/styles';
+
 
 const HomePage = () => {
+  const theme = useTheme();
+
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
       <Box display="flex" flexDirection='column' justifyContent="center" alignItems="center" minHeight="100vh">
-        <Typography variant='h2' component='h2' align='center' style={{ backgroundColor: '#2D9596', padding: '15px', width: '65%', borderRadius: '25px' , color: '#F1FADA', fontSize: '56px', fontStyle: 'normal', fontWeight: '600'}}>
+        <Typography variant='h2' component='h2' align='center' style={{ backgroundColor: theme.palette.secondary.main, padding: '15px', width: '65%', borderRadius: '25px' , color: '#F1FADA', fontSize: '56px', fontStyle: 'normal', fontWeight: '600'}}>
           Code Translator
         </Typography>
 
         <Typography paragraph='true' style={{ backgroundColor: '#2D9596', padding: '25px', marginTop: '20px', marginBottom: '0px', width: '65%', borderRadius: '25px', color: '#F1FADA', fontSize: '28px', fontStyle: 'normal', fontWeight: '300'}}>
           This tool uses an AI model to convert your code from one programming language to another. We provide support for a variety of languages such as:
 
-          <List dense='true' disablePadding='true' sx={{ listStyleType: 'disc', pl: 4, marginTop: '5px', marginBottom: '0px'}}>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+          <List dense disablePadding sx={{ listStyleType: 'disc', pl: 4, marginTop: '5px', marginBottom: '0px'}}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               C++
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               C
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Java
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Python
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               JavaScript
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Assembly
             </ListItem>
           </List>
@@ -43,20 +47,20 @@ const HomePage = () => {
             Getting Started
           </Typography>
 
-          <List dense='true' disablePadding='true' sx={{ listStyle: 'decimal', pl: 4, marginTop: '5px', marginBottom: '0px'}}>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+          <List dense disablePadding sx={{ listStyle: 'decimal', pl: 4, marginTop: '5px', marginBottom: '0px'}}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Click on the Translate button in the NavBar above
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Select the language of the source code and the language of the translated code using the two dropdowns
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Paste source code in the text editor
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Click the translate button
             </ListItem>
-            <ListItem dense='true' disablePadding='true' sx={{ display: 'list-item' }}>
+            <ListItem dense disablePadding sx={{ display: 'list-item' }}>
               Make sure to give positive/negative feedback!
             </ListItem>
           </List>
