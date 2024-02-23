d84ef30b3755e53eb62992e0f495e993b83ff81d
TaherMAfini <tma29@njit.edu>
2024-02-22 19:29:04 -0500

    Add tests for home page buttons

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,19 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
 import { useAuth } from 'src/auth'
 
 const HomePage = () => {
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated, signUp, logIn, logOut, userMetadata } = useAuth()
 
 
   return (
     <>
+      {/* MetaTags, h1, paragraphs, etc. */}
       <p>{JSON.stringify({ isAuthenticated })}</p>
+      { isAuthenticated && <p>Email: {userMetadata.email}</p>}
+      <button onClick={signUp}>sign up</button>
+      <button onClick={logIn}>Log In</button>
+      <button onClick={logOut}>Log Out</button>
     </>
   )
 }
--- web/src/pages/HomePage/HomePage.test.jsx
+++ web/src/pages/HomePage/HomePage.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen, waitFor } from '@redwoodjs/testing/web'
 
 import HomePage from './HomePage'
 
@@ -10,5 +10,35 @@ describe('HomePage', () => {
     expect(() => {
       render(<HomePage />)
     }).not.toThrow()
+  })
+})
+
+describe('Login button renders', () => {
+  it('renders successfully', () => {
+    render(<HomePage />)
+
+    const login = screen.getByText("Log In")
+
+    expect(login).toBeVisible()
+  })
+})
+
+describe('Logout button renders', () => {
+  it('renders successfully', () => {
+    render(<HomePage />)
+
+    const logout = screen.getByText("Log Out")
+
+    expect(logout).toBeVisible()
+  })
+})
+
+describe('Sign Up button renders', () => {
+  it('renders successfully', () => {
+    render(<HomePage />)
+
+    const signup = screen.getByText("sign up")
+
+    expect(signup).toBeVisible()
   })
 })
