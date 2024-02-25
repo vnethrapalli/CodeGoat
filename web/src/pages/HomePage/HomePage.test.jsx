fc56e6efa7b434a4b60ccb0d584034b1ebc349a6
TaherMAfini <tma29@njit.edu>
2024-02-25 14:44:05 -0500

    Add test for successful login

--- web/src/pages/HomePage/HomePage.test.jsx
+++ web/src/pages/HomePage/HomePage.test.jsx
@@ -1,9 +1,23 @@
 import { render, screen, waitFor } from '@redwoodjs/testing/web'
 
 import HomePage from './HomePage'
+import { useAuth } from 'src/auth'
+import { mocked } from "jest-mock";
 
-//   Improve this test with help from the Redwood Testing Doc:
+  //   Improve this test with help from the Redwood Testing Doc:
 //   https://redwoodjs.com/docs/testing#testing-pages-layouts
+
+const user = {
+  email: "tma29@njit.edu",
+  email_verified: true,
+  sub: "google-oauth2|12345678901234"
+};
+
+jest.mock("@auth0/auth0-spa-js");
+
+const mockedUseAuth0 = mocked(useAuth, true);
+
+console.log(mockedUseAuth0.toString())
 
 describe('HomePage', () => {
   it('renders successfully', () => {
@@ -40,5 +54,35 @@ describe('Sign Up button renders', () => {
     const signup = screen.getByText("sign up")
 
     expect(signup).toBeVisible()
+  })
+})
+
+describe('Login works correctly', () => {
+
+  beforeEach(() => {
+    mockedUseAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn(),
+      getAccessTokenWithPopup: jest.fn(),
+      getAccessTokenSilently: jest.fn(),
+      getIdTokenClaims: jest.fn(),
+      loginWithPopup: jest.fn(),
+      isLoading: false,
+    });
+  })
+
+  it('Login successful', async () => {
+
+    const { isAuthenticated } = mockedUseAuth0()
+    render(<HomePage />)
+
+    const login = screen.getByText("Log In")
+
+    await login.click()
+
+    expect(isAuthenticated).toBeTruthy()
+
   })
 })
