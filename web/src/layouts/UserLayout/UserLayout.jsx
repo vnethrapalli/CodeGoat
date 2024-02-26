8bcb24c9568313f391179d9d53d082b459cfa9d0
Rushi A <69522459+RushiAth@users.noreply.github.com>
2024-02-26 11:59:14 -0500

    found simpler solution to fixing bug

--- web/src/layouts/UserLayout/UserLayout.jsx
+++ web/src/layouts/UserLayout/UserLayout.jsx
@@ -1,13 +1,11 @@
 import { routes } from '@redwoodjs/router'
 
 import { AppBar, Link } from '@mui/material';
-import { deepmerge } from "@mui/utils";
 import CssBaseline from "@mui/material/CssBaseline";
-import { shouldSkipGeneratingVar as muiShouldSkipGeneratingVar, Experimental_CssVarsProvider as CssVarsProvider, ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme, experimental_extendTheme as extendTheme } from "@mui/material/styles";
 import IconButton from '@mui/material/IconButton';
 import LightMode from '@mui/icons-material/LightMode';
 import DarkMode from '@mui/icons-material/DarkMode';
-import { extendTheme as extendJoyTheme, shouldSkipGeneratingVar as joyShouldSkipGeneratingVar, } from "@mui/joy/styles";
 
 const themeLight = createTheme({
   palette: {
@@ -25,7 +23,6 @@ const themeLight = createTheme({
     },
   },
 
-  variants: {},
 });
 
 const themeDark = createTheme({
@@ -42,20 +39,18 @@ const themeDark = createTheme({
     text: {
       primary: "#F1FADA"
     }
-  },
+  }
 
-  variants: {},
+
 });
-
-const joyTheme = extendJoyTheme({ cssVarPrefix: "mui" });
 
 const UserLayout = ({ children }) => {
   const [light, setLight] = React.useState(true);
 
   return (
     <>
-      <CssVarsProvider theme={light ? deepmerge(joyTheme, themeLight) : deepmerge(joyTheme, themeDark)} shouldSkipGeneratingVar={keys => muiShouldSkipGeneratingVar(keys) || joyShouldSkipGeneratingVar(keys)}>
-        {/* <CssBaseline /> */}
+      <ThemeProvider theme={light ? themeLight : themeDark}>
+        <CssBaseline />
         <AppBar position="sticky" sx={{ background: '#265073', marginBottom: '20px', height: '10%' }}>
           <h1>
             <Link href={routes.home()} underline="none" sx={{ marginLeft: "25px", color: "#F1FADA" }}>
@@ -69,7 +64,7 @@ const UserLayout = ({ children }) => {
         </AppBar>
 
         <main>{children}</main>
-      </CssVarsProvider>
+      </ThemeProvider>
     </>
   )
 }
