78c1665ccfb7c3161e05b78f7d7a92dc54f0f046
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2024-03-14 19:16:25 -0400

    first chat gpt response!
    woooo

--- api/src/services/translation/translation.js
+++ api/src/services/translation/translation.js
@@ -1,18 +1,24 @@
-// import OpenAI from "openai";
+import OpenAI from "openai";
 
 export const getTranslation = async ({ code, inLang, outLang }) => {
-  // const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-  // const completion = await openai.chat.completions.create({
-  //   messages: [{ role: "system", content: "You are a helpful assistant." }],
-  //   model: "gpt-3.5-turbo",
-  //   max_tokens: 8
-  // });
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+    max_tokens: 8
+  });
 
-  // console.log(completion.choices[0]);
   return {
-    translation: `// converting '${code}' from ${inLang} to ${outLang}`,
+    translation: JSON.stringify(completion.choices[0]),
     inputLang: `${inLang}`,
     outputLang: `${outLang}`,
   }
+
+  // console.log(completion.choices[0]);
+  // return {
+  //   translation: `// converting '${code}' from ${inLang} to ${outLang}`,
+  //   inputLang: `${inLang}`,
+  //   outputLang: `${outLang}`,
+  // }
 }
