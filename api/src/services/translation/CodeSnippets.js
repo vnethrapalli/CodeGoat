8766ebe8a9fc886b5ee256690de88379f91981f1
rahavdayan <rd57@njit.edu>
2024-04-13 00:10:01 -0400

    code tests written

--- api/src/services/translation/CodeSnippets.js
+++ api/src/services/translation/CodeSnippets.js
@@ -1,114 +1,292 @@
+export const c = `
+#include <stddef.h>
+#include <stdio.h>
+static const int values[] = { 1, 2, 4, 9, 15 };
+#define ARRAYSIZE(x) (sizeof x/sizeof x[0])
+int main (int argc, char *argv[])
+{
+    size_t i;
+    for (i = 0; i < ARRAYSIZE(values); i++)
+    {
+        printf("%d ", values[i]);
+    }
+
+    return 0;
+}`;
+
+export const cpp = `
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    // run a loop from 1 to 10
+    // print the multiplication table
+    for (int i = 1; i <= 10; ++i) {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}
+`;
+
+export const csharp = `
+// Stopwatch solution
+using System;
+using System.Diagnostics;
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+// example snippet
+var result = new List<(int, int)>();
+foreach (int a in new[] { 1, 3, 5 })
+{
+    foreach (int b in new[] { 2, 4, 6 })
+    {
+        result.Add((a, b));
+    }
+}
+
+stopwatch.Stop();
+Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
+
+// BenchmarkDotNet solution
+// Install the BenchmarkDotNet NuGet package to use this solution
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+public class MyBenchmark
+{
+    [Benchmark]
+    public List<(int, int)> TestSnippet()
+    {
+        var result = new List<(int, int)>();
+        foreach (int a in new[] { 1, 3, 5 })
+        {
+            foreach (int b in new[] { 2, 4, 6 })
+            {
+                result.Add((a, b));
+            }
+        }
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<MyBenchmark>();
+    }
+}
+`;
+
 export const go = `
-      package main
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Example() {
+	// Create a slice of the strings that you want to join together
+	strs := []string{"aa", "bb", "cc"}
+	//Use the strings.Join function to join them, by passing the desired separator
+	fmt.Println(strings.Join(strs, "-"))
+	fmt.Println(strings.Join(strs, ", "))
+	fmt.Println(strings.Join(strs, ""))
+	// Output:
+	// aa-bb-cc
+	// aa, bb, cc
+	// aabbcc
+}
+`;
+
+export const java = `
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+
+public void postMail( String recipients[ ], String subject, String message , String from) throws MessagingException
+{
+    boolean debug = false;
+
+     //Set the host smtp address
+     Properties props = new Properties();
+     props.put("mail.smtp.host", "smtp.example.com");
+
+    // create some properties and get the default Session
+    Session session = Session.getDefaultInstance(props, null);
+    session.setDebug(debug);
+
+    // create a message
+    Message msg = new MimeMessage(session);
+
+    // set the from and to address
+    InternetAddress addressFrom = new InternetAddress(from);
+    msg.setFrom(addressFrom);
+
+    InternetAddress[] addressTo = new InternetAddress[recipients.length];
+    for (int i = 0; i < recipients.length; i++)
+    {
+        addressTo[i] = new InternetAddress(recipients[i]);
+    }
+    msg.setRecipients(Message.RecipientType.TO, addressTo);
 
 
-      import (
-          "errors"
-          "testing"
-      )
+    // Optional : You can also set your custom headers in the Email if you Want
+    msg.addHeader("MyHeaderName", "myHeaderValue");
+
+    // Setting the Subject and Content Type
+    msg.setSubject(subject);
+    msg.setContent(message, "text/plain");
+    Transport.send(msg);
+}
+`;
+
+export const javascript = `
+console.log('Script start');
+
+setTimeout(() => console.log('setTimeout()'), 0);
+
+Promise.resolve()
+  .then(() => console.log('Promise.then() #1'))
+  .then(() => console.log('Promise.then() #2'));
+
+console.log('Script end');
+
+// LOGS:
+//   Script start
+//   Script end
+//   Promise.then() #1
+//   Promise.then() #2
+//   setTimeout()
+`;
+
+export const kotlin = `
+class Turtle {
+    fun penDown()
+    fun penUp()
+    fun turn(degrees: Double)
+    fun forward(pixels: Double)
+}
+val myTurtle = Turtle()
+with(myTurtle) { //draw a 100 pix square
+    penDown()
+    for(i in 1..4) {
+        forward(100.0)
+        turn(90.0)
+    }
+    penUp()
+}
+`;
+
+export const php = `
+$zip = new ZipArchive;
+$file = $zip->open('file.zip');
+if ($file) {
+	$zip->extractTo('/extract_path/');
+	$zip->close();
+	echo 'Archive extracted successfully!';
+}
+`;
+
+export const python = `
+def merge(*args, missing_val = None):
+#missing_val will be used when one of the smaller lists is shorter tham the others.
+#Get the maximum length within the smaller lists.
+  max_length = max([len(lst) for lst in args])
+  outList = []
+  for i in range(max_length):
+    result.append([args[k][i] if i < len(args[k]) else missing_val for k in range(len(args))])
+  return outList
+`;
+
+export const ruby = `
+# Getters 'get' stuff from your class
+# Setters 'set' stuff in your class
+class Product
+  # Always Initialize It First
+  def initialize( description, price)
+    @id = rand(100...999)
+    @description = description
+    @price = price
+  end
+
+  # Create GETTER for our descrition
+  def description
+  	# don't need "return", but I like it
+    return @description
+  end
+
+  # Create SETTER for our description...Setters use = and can be named after its getter
+  def description=( description )
+    @description = description
+  end
 
 
-      // https://en.wikipedia.org/wiki/Hamming_distance:
-      // Τhe Hamming distance between two strings of equal length
-      // is the number of positions at which the corresponding symbols are different.
-      // In other words, it measures the minimum number of substitutions
-      // required to change one string into the other
-      //
-      // Hamming distance function calculates the rune based
-      // hamming distance between strings a and b
-      func HammingDistance(a string, b string) (int, error) {
-          // stings in Go are slices of bytes so we have first to convert them to runes
-          // read more here https://blog.golang.org/strings
-          ra := []rune(a)
-          rb := []rune(b)
-          if len(ra) != len(rb) {
-              return 0, errors.New("strings do not have the same length")
-          }
+  def to_s
+    # return by rewriting to_s :-p and add tabs with \t
+    return "#{@id}\t#{@description}\t#{@price}"
+  end
+end
 
+# Set it up... Instantiate our class
+book = Product.new( "Ruby On Rails For Web Development", 26.95 )
+book2 = Product.new( "Intro To Ruby", 25.95 )
 
-          var distance int
-          for i := range ra {
-              if rb[i] != ra[i] {
-                  distance++
-              }
-          }
-          return distance, nil
-      }
+# Call the thing!
+puts book
+puts book2
 
+# Call The Description Getter
+puts book.description
+# Call the Setter, set a different Description
+puts book.description= "I Like Cheese!"
+`;
 
-      func TestHammingDistance(t *testing.T) {
-          tests := []struct {
-              name    string
-              a       string
-              b       string
-              want    int
-              wantErr bool
-          }{
-              {
-                  name:    "no equal length",
-                  a:       "abc",
-                  b:       "abcd",
-                  want:    0,
-                  wantErr: true,
-              },
-              {
-                  name:    "same strings ascii",
-                  a:       "abcd",
-                  b:       "abcd",
-                  want:    0,
-                  wantErr: false,
-              },
-              {
-                  name:    "one character different ascii",
-                  a:       "Abcd",
-                  b:       "abcd",
-                  want:    1,
-                  wantErr: false,
-              },
-              {
-                  name:    "all characters different ascii",
-                  a:       "ABCD",
-                  b:       "abcd",
-                  want:    4,
-                  wantErr: false,
-              },
-              {
-                  name:    "same strings utf8",
-                  a:       "Καλημέρα",
-                  b:       "Καλημέρα",
-                  want:    0,
-                  wantErr: false,
-              },
-              {
-                  name:    "one character different utf8",
-                  a:       "Καλημέρα",
-                  b:       "Καλημερα",
-                  want:    1,
-                  wantErr: false,
-              },
-              {
-                  name:    "all characters different utf8",
-                  a:       "ΚΑΛΗΜΕΡΑ",
-                  b:       "καλημέρα",
-                  want:    8,
-                  wantErr: false,
-              },
-          }
+export const rust = `
+fn main() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
 
+    println!("Before mutation: {}", mutable_binding);
 
-          for _, tt := range tests {
-              t.Run(tt.name, func(t *testing.T) {
-                  got, err := HammingDistance(tt.a, tt.b)
-                  if (err != nil) != tt.wantErr {
-                      t.Errorf(
-                          "HammingDistance(%s %s) error = %v, wantErr %v", tt.a, tt.b, err, tt.wantErr,
-                      )
-                      return
-                  }
-                  if got != tt.want {
-                      t.Errorf("HammingDistance(%s %s) = %v, want %v", tt.a, tt.b, got, tt.want)
-                  }
-              })
-          }
-      }
-    `;
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error!
+    _immutable_binding += 1;
+    // FIXME ^ Comment out this line
+}
+`;
+
+export const typescript = `
+interface MyInterface {
+    id: number;
+    name: string;
+    properties: string[];
+  }
+
+  const myObject: MyInterface = {
+    id: 1,
+    name: 'foo',
+    properties: ['a', 'b', 'c']
+  };
+
+  function getValue(value: keyof MyInterface) {
+    return myObject[value];
+  }
+
+  getValue('id'); // 1
+  getValue('count')
+`;
--- api/src/services/translation/translation.test.js
+++ api/src/services/translation/translation.test.js
@@ -1,62 +1,104 @@
-import { getTranslation } from './translation'
+import { c, cpp, csharp, go, java, javascript, kotlin, php, python, ruby, rust, typescript } from './codeSnippets';
+import { getLanguage } from './translation';
 
-// const mockCreate = jest.fn().mockResolvedValue({
-//   choices: [{ message: { "content" : "This is a sample response!" } }]
-// });
+const MILLIS_PER_SEC = 1000;
 
-const mockCreate = jest.fn().mockImplementationOnce(async () => {
-  return {
-    choices: [{ message: { "content" : "This is a sample response!" } }]
-  }
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'authentication_error' }
-  throw err;
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'authentication_error' }
-  throw err;
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'invalid_request_error' }
-  throw err;
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'rate_limit_error' }
-  throw err;
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'tokens_exceeded_error' }
-  throw err;
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'not_found_error' }
-  throw err;
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'server_error' }
-  throw err;
-})
-.mockImplementationOnce(async () => {
-  const err = { type: 'permission_error' }
-  throw err;
+describe("testing GPT for variety of structures/languages", () => {
+  // test('c', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: c, inLang: "c", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('cpp', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: cpp, inLang: "cpp", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('csharp', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: csharp, inLang: "csharp", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('go', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: go, inLang: "go", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('java', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: java, inLang: "java", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('javascript', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: javascript, inLang: "javascript", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('kotlin', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: kotlin, inLang: "kotlin", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('php', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: php, inLang: "php", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('python', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: python, inLang: "python", outLang: "javascript" });
+  //   expect(getLanguage(response.body.translation)).toBe("javascript");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('ruby', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: ruby, inLang: "ruby", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('rust', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: rust, inLang: "rust", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
+
+  // test('typescript', async () => {
+  //   const { getTranslation } = require('./translation');
+  //   const response = await getTranslation({ code: typescript, inLang: "typescript", outLang: "python" });
+  //   expect(getLanguage(response.body.translation)).toBe("python");
+  // }, MILLIS_PER_SEC * 60);
 });
-
-jest.mock('openai', () => {
-  return jest.fn().mockImplementation(() => {
-    return {
-      chat: {
-        completions: {
-          create: mockCreate
-        }
-      }
-    };
-  });
-});
-
 
 describe("testing valid openai api key", () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
   test("valid openai api key response", async () => {
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                return {
+                  choices: [{ message: { "content" : "This is a sample response!" } }]
+                }
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toBe(200);
     expect(response.body.translation).toEqual("This is a sample response!");
@@ -65,8 +107,9 @@ describe("testing valid openai api key", () => {
 
 describe("testing invalid openai api key", () => {
   const OLD_ENV = process.env;
+
   beforeEach(() => {
-    jest.resetModules() // Most important - it clears the cache
+    jest.resetModules();
     process.env = { ...OLD_ENV }; // Make a copy
   });
 
@@ -76,46 +119,179 @@ describe("testing invalid openai api key", () => {
 
   test('testing invalid openai api key numeric', async () => {
     process.env.OPENAI_API_KEY = 5;
+
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'authentication_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(401);
   });
 
   test('testing invalid openai api key bad string', async () => {
     process.env.OPENAI_API_KEY = 'sk-alkdUalsKdjalLsd';
+
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'authentication_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation  } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(401);
   });
 })
 
 describe('testing error openai api error responses', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
   test('testing handling for a bad request', async () => {
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'invalid_request_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(400);
   });
 
   test('testing handling for a rate limiting error', async () => {
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'rate_limit_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(429);
   });
 
   test('testing handling for tokens exceeded error', async () => {
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'tokens_exceeded_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(403);
   });
 
   test('testing handling for resource not found error', async () => {
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'not_found_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(404);
   });
 
   test('testing handling for server error', async () => {
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'server_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(500);
   });
 
   test('testing handling for permission error', async () => {
+    jest.doMock('openai', () => {
+      return jest.fn().mockImplementation(() => {
+        return {
+          chat: {
+            completions: {
+              create: jest.fn().mockImplementationOnce(async () => {
+                const err = { type: 'permission_error' }
+                throw err;
+              })
+            }
+          }
+        };
+      });
+    });
+    const { getTranslation } = require('./translation');
+
     const response = await getTranslation({ code: "test", inLang: "python", outLang: "java" });
     expect(response.statusCode).toEqual(405);
   });
-
 })
