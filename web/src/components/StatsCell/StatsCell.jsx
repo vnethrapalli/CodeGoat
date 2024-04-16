c79560472bcee86999a3e152792ba5f77fa3c26d
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2024-04-15 22:10:43 -0400

    good progress on chart styling and facts styling

--- web/src/components/StatsCell/StatsCell.jsx
+++ web/src/components/StatsCell/StatsCell.jsx
@@ -42,24 +42,64 @@ export const Success = ({ stats }) => {
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   return (
     <>
-      <Box sx={{ width: "70%", marginBottom: '10px', marginTop: '10px' }}>
-        <Typography data-testid='your-stats-header' align='left' sx={{ color: theme.palette.text.secondary, fontSize: '24px', fontStyle: 'normal', fontWeight: '500', marginBottom: '10px'}}>
-          Your Stats
-        </Typography>
-
+      <Box sx={{ width: "70%", marginBottom: '10px', marginTop: '20px' }}>
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <Typography data-testid='user-num-translations' align='left' sx={{ color: theme.palette.text.secondary, fontSize: '12px', fontStyle: 'normal', fontWeight: '500', marginBottom: '10px'}}>
+            <Typography data-testid='your-stats-header' align='left' sx={{ color: theme.palette.text.secondary, fontSize: '24px', fontStyle: 'normal', fontWeight: '500', marginBottom: '10px'}}>
+              Your Stats
+            </Typography>
+
+            <Divider aria-hidden="true" textAlign="left">
+              <Typography data-testid='translation-count-header' sx={{ color: theme.palette.text.secondary, fontSize: '14px', fontStyle: 'normal', fontWeight: '500'}}>
+                Number of Translations
+              </Typography>
+            </Divider>
+            <Typography data-testid='user-num-translations' align='center' sx={{ color: theme.palette.text.secondary, fontSize: '12px', fontStyle: 'normal', fontWeight: '500', marginTop: '5px', marginBottom: '5px'}}>
               {stats.count} total translations
             </Typography>
 
-            <Typography data-testid='user-mostfreq-pair' align='left' sx={{ color: theme.palette.text.secondary, fontSize: '12px', fontStyle: 'normal', fontWeight: '500', marginBottom: '10px'}}>
-              translated from {stats.favPair[0]} to {stats.favPair[1]} a whopping {stats.favPairFreq} times!
+            <Divider aria-hidden="true" textAlign="left">
+              <Typography data-testid='user-mostfreq-pair-header' sx={{ color: theme.palette.text.secondary, fontSize: '14px', fontStyle: 'normal', fontWeight: '500'}}>
+                Most Frequent Language Pair
+              </Typography>
+            </Divider>
+            <Typography data-testid='user-mostfreq-pair' align='center' sx={{ color: theme.palette.text.secondary, fontSize: '12px', fontStyle: 'normal', fontWeight: '500', marginTop: '5px', marginBottom: '5px'}}>
+              translated from {stats.favPair[0]} to {stats.favPair[1]} a whopping {stats.favPairFreq} time{stats.favPair > 1 ? 's' : ''}!
             </Typography>
+
+            <Divider aria-hidden="true" textAlign="left">
+              <Typography data-testid='user-highest-rated-pair-header' sx={{ color: theme.palette.text.secondary, fontSize: '14px', fontStyle: 'normal', fontWeight: '500'}}>
+                Highest Rated Language Pair
+              </Typography>
+            </Divider>
           </Grid>
 
-          <Grid item xs={6} sx={{ border: 1}}>
+          <Grid item xs={6}>
+            <Typography data-testid='7day-graph-title' align='center' sx={{ color: theme.palette.text.secondary, fontSize: '24px', fontStyle: 'normal', fontWeight: '500', marginBottom: '-30px'}}>
+              7-day activity
+            </Typography>
             <LineChart
+              sx={{
+                "& .MuiChartsAxis-directionY .MuiChartsAxis-line":{
+                  strokeWidth:"2",
+                  stroke: theme.palette.text.secondary
+                },
+                "& .MuiChartsAxis-directionX .MuiChartsAxis-line":{
+                  strokeWidth:"2",
+                  stroke: theme.palette.text.secondary
+                },
+                "& .MuiChartsAxis-bottom .MuiChartsAxis-tickLabel":{
+                  strokeWidth:"1",
+                  fill: theme.palette.text.secondary
+                },
+                "& .MuiChartsAxis-left .MuiChartsAxis-tickLabel":{
+                  strokeWidth:"1",
+                  fill: theme.palette.text.secondary
+                },
+                ".MuiChartsAxis-tick": {
+                  stroke: theme.palette.text.secondary
+                }
+              }}
               xAxis={[
                 {
                   scaleType: 'time',
@@ -74,6 +114,7 @@ export const Success = ({ stats }) => {
                 },
               ]}
               height={300}
+              grid={{ horizontal: true }}
             />
           </Grid>
         </Grid>
